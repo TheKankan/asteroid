adc6a3b81ab72ab967eb17da8e3c6bc59d9a3198
TheKankan <quentin.lejuez@gmail.com>
2025-05-26 16:52:58 +0200

    added bullets

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -22,6 +23,9 @@ def main():
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots,updatable,drawable)
 
 
     while True:
